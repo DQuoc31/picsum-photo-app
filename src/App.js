d8e53935e9e282632cfe5775cd217780f3d95d68
DQuoc31 <dangmaiquoc@gmail.com>
2025-11-17 21:47:44 +0700

    Fix router for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import PhotoList from './components/PhotoList.js';
 import PhotoDetail from './components/PhotoDetail.js';
 
